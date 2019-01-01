baaa54f5ad796539659d8e957f2e2f8bca419efd
Michal Gebka <michail.gebka@gmail.com>
2019-01-01 20:31:10 +0100

    cosmetic changes

--- handlers.go
+++ handlers.go
@@ -7,7 +7,7 @@ import (
 )
 
 func getAllHandler(w http.ResponseWriter, r *http.Request) {
-	log.Println("Gethering data")
+	log.Println("Gathering data")
 	persons, _ := getAll()
 	json.NewEncoder(w).Encode(persons)
 }
@@ -17,7 +17,7 @@ func createNewPersonHandler(w http.ResponseWriter, r *http.Request) {
 
 	err := json.NewDecoder(r.Body).Decode(&people)
 	if err != nil {
-		log.Println("Request body does not match model !", err)
+		log.Println("Can not save new person. Caused by: ", err)
 	} else {
 		log.Println("Creating new person")
 		id := createNewPerson(people.Name, people.Surname, people.Age, people.Hobby, people.Active)
